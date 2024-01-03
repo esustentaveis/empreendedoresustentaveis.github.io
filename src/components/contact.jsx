28e5d9ee465f9e032beaeda0aca97775374c5847
k-js <modulodigital69@gmail.com>
2024-01-03 18:14:13 -0300

    aplicação do emailjs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,27 +8,24 @@ const initialState = {
   message: "",
 };
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+  const [setState] = useState(initialState);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
-  const clearState = () => setState({ ...initialState });
-
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, email, message);
 
     /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
 
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+      .sendForm("service_ss8vqca", "template_cpp5kz4", e.target, "DGMxb_MLCAGnKzPBB")
       .then(
         (result) => {
           console.log(result.text);
-          clearState();
+          window.location.reload();
         },
         (error) => {
           console.log(error.text);
