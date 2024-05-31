a1865b9c6c80f4ef9796a75a8e4d9a562bb151b9
mateussatoh <mateussatoh@gmail.com>
2024-05-31 12:33:11 -0300

    remove red background

--- src/app/sections/services.tsx
+++ src/app/sections/services.tsx
@@ -12,7 +12,7 @@ export const Services = (props: any) => {
             abordam as principais necessidades de negócios sustentáveis.
           </p>
         </div>
-        <div className=" bg-red-500 grid grid-cols-3 grid-rows-2">
+        <div className=" grid grid-cols-3 grid-rows-2">
           {props.data
             ? props.data.map((d: any, i: any) => (
                 <div key={`${d.name}-${i}`}>
